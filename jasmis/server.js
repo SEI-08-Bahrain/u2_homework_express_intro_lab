493e6c4645e57d0302c80a8a781f5090d4bb4adb
ZahraaAlhawaj <zahraa.mu2019@gmail.com>
2024-01-28 16:40:46 +0300

    create data

--- jasmis/server.js
+++ jasmis/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome to Jasmis!<h1>')
+  res.render('home')
 })
 
 app.listen(3000, () => {
