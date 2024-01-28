2f3e45cf2da81005a22da89d506bf57c7218925b
9Jassim <jassim9.a@gmail.com>
2024-01-28 15:48:25 +0300

    complete jasmi's homework

---
+++ jasmis/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const database = require('./data/jasmi-menu')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/menuItems', (req, res) => {
+  const menu = database.getAll()
+  res.render('menuItems/index', { menu })
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
