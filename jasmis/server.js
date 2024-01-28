a6f3453dad9c422c5083a5670df1e331d64e44c0
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-01-28 20:26:25 +0300

    menu vsible

--- jasmis/server.js
+++ jasmis/server.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
+const database = require('./data/jasmis-menu')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>Welcome to Jasmis</h1>')
+// })
+
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome to Jasmis</h1>')
+  res.render('home')
+})
+
+app.get('/menuItems', (req, res) => {
+  const menu = database.getAll()
+  res.render('menuItems/index', { menu })
 })
 
 app.listen(3000, () => {
